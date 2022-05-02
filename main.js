8ac62c84212f49c0a51e68bd195c5403cdfe17e0
Nebyou Chaka <nebyouchaka@gmail.com>
2022-05-01 22:05:22 -0400

    amzaing update

--- main.js
+++ main.js
@@ -15,3 +15,34 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
+const navbar = document.getElementById('navbar');
+
+window.onscroll = function () {
+  if (window.pageYOffset > 100) {
+    navbar.classList.remove('top');
+  } else {
+    navbar.classList.add('top');
+  }
+};
+
+
+
+
+
+
+
+
+$('#navbar a, .btn').on('click', function (e) {
+    if (this.hash !== '') {
+      e.preventDefault();
+  
+      const hash = this.hash;
+  
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top - 100,
+        },
+        800
+      );
+    }
+  });
